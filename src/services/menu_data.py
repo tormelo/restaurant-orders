a1120a5ac8772e9c2d1d9f91e18d3a0c8269f7b0
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-16 15:38:37 -0300

    feat: implements MenuData class

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,25 @@
+import pandas as pd
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        df = pd.read_csv(source_path)
+
+        dishes_dict = {}
+
+        for dish, price, ingredient_name, ingredient_amount in df.itertuples(
+            index=False
+        ):
+            if dish not in dishes_dict:
+                dishes_dict[dish] = Dish(dish, price)
+
+            ingredient = Ingredient(ingredient_name)
+
+            dishes_dict[dish].add_ingredient_dependency(
+                ingredient, ingredient_amount
+            )
+
+        self.dishes = set(dishes_dict.values())
