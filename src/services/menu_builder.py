e3cbdfd8ecf9def43d3d6f7b9adb627f94288e8a
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-16 17:02:47 -0300

    feat: implements get_main_menu method of the MenuBuilder class

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,31 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        dish_names = []
+        ingredients = []
+        prices = []
+        restrictions = []
+
+        for dish in self.menu_data.dishes:
+            dish_names.append(dish.name)
+            ingredients.append(dish.get_ingredients())
+            prices.append(dish.price)
+            restrictions.append(dish.get_restrictions())
+
+        df = pd.DataFrame(
+            {
+                "dish_name": dish_names,
+                "ingredients": ingredients,
+                "price": prices,
+                "restrictions": restrictions,
+            }
+        )
+
+        dishes_bool_map = df["restrictions"].map(
+            lambda restrictions: False if restriction in restrictions else True
+        )
+
+        if not restriction:
+            return df
+        else:
+            return df.loc[dishes_bool_map]
