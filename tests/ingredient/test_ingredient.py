1ea55e17baf60f76bab348cefa8d55260a802775
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-16 14:32:16 -0300

    test: add tests to the Ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    meat = Ingredient("carne")
+    other_meat = Ingredient("carne")
+    bacon = Ingredient("bacon")
+
+    assert meat == other_meat
+    assert hash(meat) == hash(other_meat)
+    assert hash(meat) != hash(bacon)
+    assert meat.name == "carne"
+    assert meat.__repr__() == "Ingredient('carne')"
+
+    assert meat.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
